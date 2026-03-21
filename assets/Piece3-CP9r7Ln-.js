e97d291e6c1ac5128f81111607bb04307520c3c2
olivia-em <olivia-em@users.noreply.github.com>
2026-03-21 00:32:58 +0000

    deploy: 6d760281c59ca6df3a5395dcca4fc4bfb645a0e9

--- assets/Piece3-CP9r7Ln-.js
+++ assets/Piece3-CP9r7Ln-.js
@@ -1,4 +1,4 @@
-import{u as l,j as o}from"./index-Cp7yHQ-s.js";import{u as h}from"./useTrackPiece-CDP_vGp0.js";const m="_piece3Container_r2vew_8",p="_birdsContainer_r2vew_20",w="_bird_r2vew_20",f="_poemText_r2vew_78",s={piece3Container:m,birdsContainer:p,bird:w,poemText:f},_=()=>{h("lack_of_flight");const{trackLofFile:r}=l(),n=`lack of flight   
+import{u as l,j as o}from"./index-BYT0WV7o.js";import{u as h}from"./useTrackPiece-BAuRYWlQ.js";const m="_piece3Container_r2vew_8",p="_birdsContainer_r2vew_20",w="_bird_r2vew_20",f="_poemText_r2vew_78",s={piece3Container:m,birdsContainer:p,bird:w,poemText:f},_=()=>{h("lack_of_flight");const{trackLofFile:r}=l(),n=`lack of flight   
 
 I sit atop the stairs
 white carpeted and stained
