2656bac9f33e2e16f88d47fc13b1c38b0b70b658
olivia-em <olivia-em@users.noreply.github.com>
2026-04-16 22:20:04 +0000

    deploy: 2646f40cfe6772d9215353f3f85833209df332d2

---
+++ assets/Piece3-BspIZHrM.js
@@ -0,0 +1,30 @@
+import{u as l,j as o}from"./index-BnZjQ6MJ.js";import{u as h}from"./useTrackPiece-BnIjg5cJ.js";const m="_piece3Container_r2vew_8",p="_birdsContainer_r2vew_20",_="_bird_r2vew_20",w="_poemText_r2vew_78",i={piece3Container:m,birdsContainer:p,bird:_,poemText:w},y=()=>{h("lack_of_flight");const{trackLofFile:r}=l(),n=`lack of flight   
+
+I sit atop the stairs
+white carpeted and stained
+a place to wait for choices
+a subtle space for pain
+
+from here, 	
+            I see the foyer
+the front and bedroom doors
+could go both ways, they say
+(make-believe to play past four)
+
+from here, 
+              and looking down
+I'd hit my head right at the base
+I remember needing stitches
+and the lies, 	your hidden face
+
+from here, 
+                I hear the stones
+with two birds dead, I wonder why
+they ring against 	      the door
+and they echo   in my mind
+
+ten years pass, and I'm still here
+they say with everything I need
+but I stay, can't move an inch
+white carpet stains, 
+                and still I bleed`,s=["𓅪","𓅫","𓅓","✮","⋆","˚","｡","𓅨","°","✩","☁︎","☾","⁺","₊","✧"],a=(e=>e.split("").map(t=>/\s/.test(t)?t:s[Math.floor(Math.random()*s.length)]))(n),d=e=>{if(!e)return;r(e);const t=`/assets/piece3/${e}`;e.toLowerCase().endsWith(".txt")&&window.__COLLEX_OPEN_TXT__&&window.__COLLEX_OPEN_TXT__(t)||window.open(t,"_blank")},c=[{emoji:"𓅨",downloadFile:"LOF.JPG"},{emoji:"𓅩",downloadFile:"LOF.txt"},{emoji:"𓅓",downloadFile:null}];return o.jsxs("div",{className:i.piece3Container,children:[o.jsx("div",{className:i.birdsContainer,children:c.map((e,t)=>o.jsx("div",{className:i.bird,onClick:()=>d(e.downloadFile),children:e.emoji},t))}),o.jsx("pre",{className:i.poemText,children:a})]})};export{y as default};
